7319176c2ef14499d7da21fcbea63c6031b543c3
JimmerFish <jrticati@ncsu.edu>
2021-10-20 23:50:01 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+
+import * as React from 'react';
+import './App.css';
+
+
+class Line extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            fishes: ['Tuna','Marlin', 'Bass']
+        };
+    }
+
+    render() {
+        const moves = this.state.fishes.map((fishname, fish) => {
+            return (
+                <li key={fishname}>
+                    <p> {fishname} </p>
+                    <button >
+                        Learn More
+                    </button>
+                </li>
+            );
+        });
+
+        return (
+            <div className="List">
+                <ol>{moves}</ol>
+            </div>
+        );
+    }
+
+    
+}
+
+export default Line;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Line from './App';
+
+
+ReactDOM.render(
+  <Line />,
+  document.getElementById('root')
+);
